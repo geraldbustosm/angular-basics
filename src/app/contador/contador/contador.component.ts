1b024945d27d633ce199481fd3681a2dc83aaaa7
Luis Gerardo Bustos Morales <lbustosm@everis.com>
2021-09-27 23:38:43 -0300

    basic project

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-contador',
+    template: `
+            <h1>{{titulo}}</h1>
+            <h3>La base es: <strong>{{base}}</strong></h3>
+            <button (click)="acumular(-base);">- {{base}}</button>
+            <span>{{contador}}</span>
+            <button (click)="acumular(base);">+ {{base}}</button>
+        `
+})
+export class ContadorComponent {
+    titulo: string = 'Contador App';
+    contador: number = 0;
+    base: number = 5;
+
+    acumular(operacion: number) {
+        this.contador += operacion;
+    }
+}
